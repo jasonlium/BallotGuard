706994f9bb549772a065dbd97a2f5d8cf5b50b4b
eliotngv <oygktu37988@outlook.com>
2025-11-29 11:21:58 +0800

    init react page

---
+++ src/src/App.tsx
@@ -0,0 +1,23 @@
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import '@rainbow-me/rainbowkit/styles.css';
+
+import { config } from './config/wagmi';
+import { BallotApp } from './components/BallotApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider locale="en">
+          <BallotApp />
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
+
+export default App
